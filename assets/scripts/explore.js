30a4fdc92acd0dc6597bd3461d472792a6d4e1d8
lavinma <lavin.ma18@gmail.com>
2025-05-06 13:35:41 -0700

    Finished explore.js and expose.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,53 @@
 // explore.js
 
+// only starts to run when HTML content is fully loaded
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+function init() {     // gets called when everything loads (the setup function)
+  const synth = window.speechSynthesis;       // gives access to the Web Speech API so the browser can talk
+  const voiceSelect = document.getElementById('voice-select');      // gets the dropdown menu with all the different voice options
+  const speakButton = document.querySelector('button');       // gets the "Press to Talk" button
+  const textInput = document.getElementById('text-to-speak');       // gets the text box that the user can type in
+  const faceImg = document.querySelector('img');      // gets the image
+
+  let voices = [];      // stores the browser voices
+
+  // gets all the available voices and fills the dropdown menu
+  function populateVoices() {
+    voices = synth.getVoices();
+
+    voiceSelect.innerHTML = ''; // resets the menu to ensure no repeats
+    
+    voices.forEach((voice, i) => {      // loops through all the voices and adds each one to the dropdown menu
+      const option = document.createElement('option');
+      option.value = i; 
+      option.textContent = `${voice.name} (${voice.lang})`; 
+      voiceSelect.appendChild(option); 
+    });
+  
+  }
+
+  // populate voices at the start
+  populateVoices();
+
+  // runs only if voices don't load
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoices;
+  }
+
+
+  speakButton.addEventListener('click', () => {       // when the button is clicked...
+    const utter = new SpeechSynthesisUtterance(textInput.value);      // what the user wants the browser to say
+    const selectedIndex = voiceSelect.value;      // the voice the user wants
+    utter.voice = voices[selectedIndex];      // sets the browser voice to the chosen voice
+
+    faceImg.src = 'assets/images/smiling-open.png';       // changes the image to a talking face
+
+    // changes the image back at the end
+    utter.onend = () => {
+      faceImg.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utter); 
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,51 @@
 // expose.js
 
+// only starts to run when HTML content is fully loaded
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  // TODO
+function init() {     // contains the interactive logic and only gets called when HTML content is fully loaded
+  // grab all the important elements from the page
+  const hornSelect = document.getElementById("horn-select");      // gets the dropdown menu where the user chooses between air, car, or horn
+  const hornImage = document.querySelector("img");      // gets the image on the page and a new one is put in for each different horn
+  const hornAudio = document.querySelector("audio");      // the audio element - how the horn sound is played
+  const volumeSlider = document.getElementById("volume");       // gets the volume slider 
+  const volumeIcon = document.getElementById("volume-controls").querySelector("img");       // gets the speaker image
+  const playButton = document.querySelector("button");      // gets the button that plays the horn sound 
+
+  // instance of the confetti animation
+  const jsConfetti = new JSConfetti();
+
+
+  hornSelect.addEventListener("change", () => {       // everytime the user selects a horn...
+    const selected = hornSelect.value;      // get the selected value
+    hornImage.src = `assets/images/${selected}.svg`;      // get the matching image to the selected horn
+    hornAudio.src = `assets/audio/${selected}.mp3`;       // get the matching audio to the selected horn
+  });
+
+
+  volumeSlider.addEventListener("input", () => {      // when the user moves the volume slider
+    const value = Number(volumeSlider.value);       // gets the selected volume
+    hornAudio.volume = value / 100;       // converts the selected volume value to work
+
+    // updates the volume icon based on the current selected volume level
+    if (value === 0) {
+      volumeIcon.src = "assets/icons/volume-level-0.svg";
+    } else if (value < 33) {
+      volumeIcon.src = "assets/icons/volume-level-1.svg";
+    } else if (value < 67) {
+      volumeIcon.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volumeIcon.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+
+  playButton.addEventListener("click", () => {      // when the button is clicked 
+    hornAudio.play();       // plays the audio
+
+    // if the user selected the party horn, play the confetti animation
+    if (hornSelect.value === "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
 }
